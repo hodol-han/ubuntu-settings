99bfd76cb24731a67272d485a412590024623602
Hodol Han <bab6ting@gmail.com>
2025-04-13 23:40:15 +0900

    vscode: Add .bat to enable VS2019 builds
    Note both .bat and .lnk files have hard-coded individual path settings.

---
+++ annoying-vscode/launch-vscode-for-vs2019.bat
@@ -0,0 +1,4 @@
+:: launch-vscode.bat
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
+start "" "C:\Users\bab5t\AppData\Local\Programs\Microsoft VS Code\Code.exe"
